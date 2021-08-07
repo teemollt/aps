70e20629b56a46a834add5a9e0fd75bb475bf3b5
teemollt <teemollt@gmail.com>
2021-08-08 00:43:47 +0900

    bs lc278

---
+++ binary search/First Bad Version_lc278.py
@@ -0,0 +1,15 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return an integer
+# def isBadVersion(version):
+
+class Solution:
+    def firstBadVersion(self, n):
+        l, r = 1, n
+        while l <= r:
+            mid = (l+r) // 2
+            if isBadVersion(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+        return l
